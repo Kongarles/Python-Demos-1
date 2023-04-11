052ef79953fba83a137d8945effdbf1a9d1abb83
Kongarles <kqyraaa@gmail.com>
2023-04-11 22:22:38 +0300

    update

---
+++ object_oriented_programming.py
@@ -0,0 +1,13 @@
+class Employee():
+    def __init__(self, employee_name, department, starting_year, salary):
+        self.employee_name = employee_name
+        self.department = department
+        self.starting_year = starting_year
+        self.salary = salary
+
+employee_1 = Employee("Ben", "sales", "2020", "5000")
+
+print(employee_1.employee_name)
+print(employee_1.department)
+print(employee_1.starting_year)
+print(employee_1.salary)
