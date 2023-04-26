2893e9140e848ec5d991704fbc6135a5ebd4583b
Kongarles <kqyraaa@gmail.com>
2023-04-26 12:54:56 +0300

    .

--- project 5/project_5.py
+++ project 5/project_5.py
@@ -1,4 +1,4 @@
 import pandas as pd
 import numpy as np
 
-last_year = pd.read_csv("employee_revenue_lastyear.csv")
+last_year = pd.read_csv(r"C:\Users\Kayra\Masaüstü\Python_Demos\Demos\project 5\employee_revenue_lastyear.csv")
